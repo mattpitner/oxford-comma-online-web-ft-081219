d467197df91ea79f0999bdb72de56db18afe85f7
Matt Pitner <mattpitner09@gmail.com>
2019-08-15 17:53:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length <= 1 
+    return array.join
+  elsif array.length == 2 
+    return array.join(" and ")
+  elsif array.length == 3
+    array[-1].insert(0, "and ")
+    return array.join(", " )
+  else 
+      array[-1].insert(0, "and ")
+    return array.join(", " )
+  end
 end
+
